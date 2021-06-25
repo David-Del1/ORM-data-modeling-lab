728884b0ec34ac17aa1a5e37aa5a1d686cbe6b44
AngelDOrtiz <aortizghs@gmail.com>
2021-06-25 11:15:35 -0700

    second route and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 import sequelize from '../lib/utils/db.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Reviewer from '../lib/models/Reviewer.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -21,5 +22,52 @@ describe('demo routes', () => {
       updatedAt: expect.any(String),
       createdAt: expect.any(String) 
     });
+  });
+
+  it('gets all users via GET', async () => {
+    
+    const user1 = await Reviewer.bulkCreate([
+      {
+        userName: 'banana lover',
+        company: 'banana land'
+      },
+      {
+        userName: 'banana enthusiast',
+        company: 'banana republic'
+      },
+      {
+        userName: 'banana fanatic',
+        company: 'banana inc'
+      }
+    ]);
+
+    
+
+    const res = await request(app).get('/api/v1/reviewers');
+    expect(res.body).toEqual(
+      [{
+        id: expect.any(Number),
+        userName: 'banana lover',
+        company: 'banana land',
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String) 
+      },
+      {
+        id: expect.any(Number),
+        userName: 'banana enthusiast',
+        company: 'banana republic',
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String) 
+      },
+      {
+        id: expect.any(Number),
+        userName: 'banana fanatic',
+        company: 'banana inc',
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String) 
+      }],
+      
+    );
+  
   });
 });
