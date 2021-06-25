e16f66b7d5db09d64bdc3956acc03e7563295894
David Delgadillo <daviddel.731@gmail.com>
2021-06-24 17:06:48 -0700

    created reviewer model and changed pool to db

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-import pool from '../lib/utils/pool.js';
+import pool from '../lib/utils/db.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
@@ -6,5 +6,20 @@ import app from '../lib/app.js';
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/reviewer')
+      .send({
+        userName: 'Tucker',
+        company: 'NYT'
+      });
+
+    expect(res.body).toEqual({
+      id: 1,
+      userName: 'Tucker',
+      company: 'NYT'
+    });
   });
 });
